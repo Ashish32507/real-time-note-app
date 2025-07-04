53bd7a8adcc187c2e0379455fa34763fc1b8cae0
Ashish Kumar Yadav <103557142+Ashish32507@users.noreply.github.com>
2025-07-04 16:50:58 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -13,7 +13,7 @@ const server = http.createServer(app);
 // Enable CORS for frontend
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173",
+    origin: "https://real-time-note-app-indol.vercel.app",
     methods: ["GET", "POST"],
   },
 });
